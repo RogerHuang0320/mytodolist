2bbb695401790a3007d3ce3e2c528795d881cc8a
Roger <winter30009@yahoo.com.tw>
2022-05-11 21:32:43 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
+const todo = require('./models/todo')
 const app = express()
 const port = 3000
 
@@ -36,6 +37,26 @@ app.get('/todos/:id', (req, res) => {
   return Todo.findById(id)
     .lean()
     .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
