c71e1549c6903f6b9ffcd4f5bc5b155bce8be10d
Roger <winter30009@yahoo.com.tw>
2022-05-20 12:48:54 +0800

    "refactor: modify mongoose config"

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
