7f33c2c8cf981b8b6f898ac85aec646228a7be39
Roger <winter30009@yahoo.com.tw>
2022-05-20 10:21:19 +0800

    add sort function

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort()
+    .sort({ name: 'asc' }) //desc
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板, 後面這裡就是key跟value的代號都一樣,都是todos, 原樣為{todos: todos}
     .catch(error => console.log(error)) // 錯誤處理
 })
