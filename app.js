f42dbb9ebf4be784e438e619a964935ff771528e
Roger <winter30009@yahoo.com.tw>
2022-05-20 10:17:25 +0800

    add checkbox function

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort()
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板, 後面這裡就是key跟value的代號都一樣,都是todos, 原樣為{todos: todos}
     .catch(error => console.log(error)) // 錯誤處理
 })
@@ -50,10 +51,11 @@ app.get('/todos/:id/edit', (req, res) => {
 
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === "on"
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
