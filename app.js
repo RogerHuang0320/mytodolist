7a0e960fb312b1bc83cd2734bb64336a59b67d3f
Roger <winter30009@yahoo.com.tw>
2022-05-28 21:10:14 +0800

    "feat: add express-session"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -12,6 +13,11 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.listen(PORT, () => {
   console.log(`App is now running on http://localhost:${PORT}`)
